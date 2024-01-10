7b1c68e53663795a074027d82df9c9de240b8cb7
Justin Brooks <justin.brooks@cfacorp.com>
2024-01-10 09:00:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/suppresslintwoe/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.suppresslintwoe
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.suppresslintwoe.ui.theme.SuppressLintWoeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuppressLintWoeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("ComposeModifierMissing")
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuppressLintWoeTheme {
+        Greeting("Android")
+    }
+}
